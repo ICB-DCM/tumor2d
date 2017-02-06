6ea0d4276d135da1bff500361e761982181f54e6
neuralyzer <emmanuel.klinger@gmail.com>
2017-02-06 14:13:17 +0100

    make an openmp flag and deactivate it per default

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ from setuptools import setup, Extension, find_packages
 from glob import glob
 from subprocess import run, PIPE
 
+ENABLE_OPENMP = False
+
+# I think this build ext first
 class Build_ext_first(setuptools.command.install.install):
     def run(self):
         self.run_command("build_ext")
@@ -17,15 +20,24 @@ gsl_cflags = (run(["gsl-config", "--cflags"], stdout=PIPE)
 
 gsl_include = [flag[2:] for flag in gsl_cflags if flag.startswith("-I")]
 
+
+if ENABLE_OPENMP:
+    omp_liraries = ["gomp"]
+    omp_compile_args = ["-fopenmp"]
+else:
+    omp_liraries = []
+    omp_compile_args = []
+
+
 tumor2d_ext = Extension(
     name='_nixTumor2d',
     sources=(['tumor2d/src/tumor_if.i']
              + [f for f in glob("tumor2d/src/*.cpp")
                 if f != "tumor2d/src/tumor_if_wrap.cpp"]),
-    libraries=["gsl", "gslcblas", "m", "gomp"],
+    libraries=["gsl", "gslcblas", "m"] + omp_liraries,
     swig_opts=['-c++', '-modern'],
     include_dirs=gsl_include,
-    extra_compile_args=["-DDIMENSIONS=2", "-fopenmp"])
+    extra_compile_args=["-DDIMENSIONS=2"] + omp_compile_args)
 
 setup(
     ext_modules=[tumor2d_ext],
