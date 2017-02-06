e59a739e7886f207f7bc5fc17e8b1fb111698c7f
neuralyzer <emmanuel.klinger@gmail.com>
2017-02-06 13:29:41 +0100

    read gsl compiler flags

--- setup.py
+++ setup.py
@@ -2,12 +2,14 @@ import setuptools
 import setuptools.command.install
 from setuptools import setup, Extension, find_packages
 from glob import glob
-
+from subprocess import run, PIPE
 
 class Build_ext_first(setuptools.command.install.install):
     def run(self):
         self.run_command("build_ext")
         return setuptools.command.install.install.run(self)
+
+gsl_cflags = run(["gsl-config", "--cflags"], stdout=PIPE).stdout.decode()
 
 
 tumor2d_ext = Extension(
@@ -15,9 +17,9 @@ tumor2d_ext = Extension(
     sources=(['tumor2d/src/tumor_if.i']
              + [f for f in glob("tumor2d/src/*.cpp")
                 if f != "tumor2d/src/tumor_if_wrap.cpp"]),
-    libraries=["gsl", "gslcblas"],
-    swig_opts=['-c++', '-modern', '-I../include'],
-    extra_compile_args=["-DDIMENSIONS=2"])
+    libraries=["gsl", "gslcblas", "m"],
+    swig_opts=['-c++', '-modern'],
+    extra_compile_args=["-DDIMENSIONS=2"] + gsl_cflags.split(" "))
 
 setup(
     ext_modules=[tumor2d_ext],
