acc3b0a83f1976b2bf42b1e43c5e12213f93b959
neuralyzer <emmanuel.klinger@gmail.com>
2017-02-21 08:26:45 +0100

    introduce parameter variation effect test

--- test/test_executable.py
+++ test/test_executable.py
@@ -8,20 +8,3 @@ def test_result_non_empty():
     assert len(sim['proliferation_profile']) > 0
     assert sim['growth_curve'][0] < sim['growth_curve'][-1]
 
-
-def test_same_seed_same_result():
-    sim1 = simulate(randseed=1)
-    sim2 = simulate(randseed=1)
-    assert (sim1['extra_cellular_matrix_profile'] == sim2['extra_cellular_matrix_profile']).all()
-    assert (sim1['growth_curve'] == sim2['growth_curve']).all()
-    assert (sim1['proliferation_profile'] == sim2['proliferation_profile']).all()
-
-
-def test_different_seed_different_result():
-    sim1 = simulate(randseed=1)
-    sim2 = simulate(randseed=2)
-
-    assert ((sim1['extra_cellular_matrix_profile'] != sim2['extra_cellular_matrix_profile']).any()
-            or (sim1['growth_curve'] != sim2['growth_curve']).any()
-            or (sim1['proliferation_profile'] != sim2['proliferation_profile']).any()
-            )
--- test/test_stored_pars.py
+++ test/test_stored_pars.py
@@ -2,10 +2,10 @@ import pytest
 import os
 from .pars import pars
 import numpy as np
-from tumor2d._tumorutil import tumor2d_simulate
+from tumor2d import simulate
+import copy
 
-
-@pytest.fixture(params=list(range(4)))
+@pytest.fixture(params=list(range(1)))
 def par_and_gt(request):
     base = os.path.join(os.path.dirname(__file__), "test_data")
     n = request.param
@@ -14,9 +14,19 @@ def par_and_gt(request):
     return (par, data)
 
 
-def test_stored(par_and_gt):
+def test_reproduce_stored(par_and_gt):
     par, data = par_and_gt
-    res = tumor2d_simulate(**par)
-    assert (res.growth_curve == data["gc"]).all()
-    assert (res.extra_cellular_matrix == data["ecm"]).all()
-    assert (res.proliferation == data["proliferation"]).all()
+    res = simulate(**par)
+    for key in data:
+        assert (res[key] == data[key]).all()
+
+
+def test_parameter_variation(par_and_gt):
+    par, data = par_and_gt
+    for key in par:
+        par_cp = copy.copy(par)
+        par_cp[key] *= 2
+        res = simulate(**par_cp)
+        for key_data in data:
+            assert (res[key_data] != data[key_data]).any(),\
+                "Parameter {} has no influence".format(key)
